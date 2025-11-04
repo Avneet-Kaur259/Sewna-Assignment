aa9403c14f6a961a710b71feb23e1012ee024426
Avneet Kaur <avneet2528@gmail.com>
2025-11-04 21:43:24 +0530

    api done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 
 import { connectDB } from "./config/db.js";
-import authRoutes from "./routes/auth.route.js";
+import designRoutes from "./routes/design.route.js";
+
 
 dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
-
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth", authRoutes);
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }));
+}
+app.use(express.json());
+
+app.use("/api/design", designRoutes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
